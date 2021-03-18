26fe858c3b942b270b3476e9faf3e2caaa8590d5
Filip Condac <55697885+FilipCondac@users.noreply.github.com>
2021-03-18 14:19:05 +0000

    Delete draw-app directory

--- draw-app/comments and report.js
+++
@@ -1,5 +0,0 @@
-//So basically we dont want to fill in the shape as we draw it, and we cant manage to finish shape, edit shape 
-
-//eraser resets all colours to begin with to white for all other tools unless you select again the color on color palette how to change that 
-
-//import picture how to change the size using mouse pressed 
